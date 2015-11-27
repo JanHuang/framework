36c26c025905e586073f5249a62591cac45640e9
janhuang <384099566@qq.com>
2015-11-27 14:44:59 +0800

    update component to 2.0

---
+++ src/FastD/Framework/BootDev.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janhuang
+ * Date: 15/11/27
+ * Time: 下午12:31
+ * Github: https://www.github.com/janhuang
+ * Coding: https://www.coding.net/janhuang
+ * SegmentFault: http://segmentfault.com/u/janhuang
+ * Blog: http://segmentfault.com/blog/janhuang
+ * Gmail: bboyjanhuang@gmail.com
+ * WebSite: http://www.janhuang.me
+ */
+
+namespace FastD\Framework;
+
+/**
+ * AppKernel boot in product.
+ *
+ * Class BootDev
+ *
+ * @package FastD\Framework
+ */
+class BootDev extends BootProd
+{
+    public function boot()
+    {
+
+    }
+}
